315bf561145e602f9e4eeedbe0407becbc36d824
Ingnaryk <ingnaryk@outlook.com>
2022-10-29 05:47:08 +0800

    changed

--- include/Array.h
+++ include/Array.h
@@ -1,3 +1,13 @@
+template <typename T>
 class Array
 {
+private:
+    T *data;
+
+public:
+    Array(unsigned int size);
+    Array(const Array &otherArray);
+    /////////////
+    typedef void (*callback)(T value, unsigned int index, Array<T> *thisArray);
+    void forEach(callback c);
 };
