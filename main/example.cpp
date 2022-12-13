08d6a511d29f149e171fad0aa904cf915091bec4
Ingnaryk <116889289+Ingnaryk@users.noreply.github.com>
2022-12-13 11:20:31 +0800

    Update example.cpp

--- main/example.cpp
+++ main/example.cpp
@@ -5,14 +5,6 @@
 #include <random>
 using namespace std;
 
-/* implement part of ECMAScript6.0 features by C++
-** on the one hand, to absorb modern grammer skills and thoughts
-** on the other hand, to learn new C++(11~20) featrues like std::variant, std::initializer_list, std::fucntion, std::optional, remove_pointer, variadic templates and so on.
-**
-** because of the strongly typed featre of C++,
-** The assignment between objects may be of deep copy.
-*/
-
 template <typename T>
 void test(T &arr)
 {
