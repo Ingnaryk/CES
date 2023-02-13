dcd873b088c7d477b289bff25d6c05298837544a
Ingnaryk <116889289+Ingnaryk@users.noreply.github.com>
2023-02-14 00:21:06 +0800

    Update timer.h

--- inc/timer.h
+++ inc/timer.h
@@ -3,14 +3,16 @@
 
 #include <chrono>
 
+using namespace std::chrono_literals;
+
 struct timer
 {
     using millisecond = std::chrono::duration<float, std::milli>;
     const char *name;
     std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
-    Timer(const char *);
-    Timer(const Timer &) = delete;
-    ~Timer();
+    timer(const char *);
+    timer(const timer &) = delete;
+    ~timer();
 };
 
 #endif // _TIMER_H_
